c44c5fe8fbaeaabec4efa8ba8c7c2542ee8442b0
Nika <danzi6386@gmail.com>
2025-03-17 15:51:05 +0200

    fixed dark mode

--- src/app/core/pipes/split.pipe.ts
+++ src/app/core/pipes/split.pipe.ts
@@ -7,6 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SplitPipe implements PipeTransform {
 	transform(value: string, index = 0, devider = ':'): unknown {
 		const arr = value.split(devider);
+
 		return arr.length > index ? arr[index] : '';
 	}
 }
